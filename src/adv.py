8d084df9a21edf7cd11f0891c9326afd45c20db1
Jason Prince <jasoncprince@gmail.com>
2019-12-05 10:02:58 -0700

    adv is working

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,14 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -49,3 +39,31 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+valid_direction = ("n", "s", "e", "w")
+
+current_player = Player("Jason", 42, room['outside'])
+
+while True:
+    print()
+    print(f'Your current location is {current_player.current_room.name}')
+    print()
+    print(f'\n{current_player.current_room.description}')
+    print()
+    direction = input(f'Please choose a direction: ')
+    print()
+    if direction.lower() == "n":
+        current_player.current_room = current_player.current_room.n_to
+    elif direction.lower() == "s":
+        current_player.current_room = current_player.current_room.s_to
+    elif direction.lower() == "e":
+        current_player.current_room = current_player.current_room.e_to
+    elif direction.lower() == "w":
+        current_player.current_room = current_player.current_room.w_to
+    elif direction.lower() == "q":
+        print()
+        print(f'{current_player.name}, thanks for playing.')
+        break
+    else:
+        print()
+        print("Please choose n, s, e, or w as direction.")
--- src/player.py
+++ src/player.py
@@ -1,23 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+
 class Player():
     """ A player, their race and location """
-    
-    def __init__(self, name, race, current_room):
+
+    def __init__(self, name, age, current_room):
         self.name = name
-        self.race = race
+        self.age = age
         self.current_room = current_room
-    
-    def __str__(self):
-        return(f"{self.name} is of the {self.race} race and is in {self.current_room}")
-    
-    def move(self, direction):
-        if direction == "n":
-            self.current_room = self.current_room.n_to
-        elif direction == "e":
-            self.current_room = self.current_room.e_to
-        elif direction == "w":
-            self.current_room = self.current_room.w_to
-        elif direction == "s":
-            self.current_room = self.current_room.s_to
