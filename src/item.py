06d531d2952724d774841eb9c9551c0d423aa894
Jason Prince <jasoncprince@gmail.com>
2019-12-05 10:30:02 -0700

    created item class

---
+++ src/item.py
@@ -0,0 +1,8 @@
+
+class Item():
+    """Items that are in the rooms and player can carry"""
+
+    def __init__(self, sword, duck, clock):
+        self.sword = sword
+        self.duck = duck
+        self.clock = clock
